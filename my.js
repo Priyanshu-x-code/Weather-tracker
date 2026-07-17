092bd30e9050d310ac0ae27fdd6165629dfadbea
Priyanshu-x-code <priyanshu.dhiman.1829@gmail.com>
2026-07-17 04:52:16 -0700

    add

---
+++ my.js
@@ -0,0 +1,3 @@
+    const geoApi ="https://geocoding-api.open-meteo.com/v1/search?name=London&count=1";
+    const weaApi ="https://api.open-meteo.com/v1/forecast?latitude=51.5074&longitude=-0.1278&current=temperature_2m,relative_humidity_2m,apparent_temperature,weather_code,wind_speed_10m,is_day&timezone=auto";
+    const  
